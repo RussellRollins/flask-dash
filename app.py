b29b994a215cb5b4dbea8d4b12dd631029ece871
Russell Rollins <russell.rollins@salesforce.com>
2015-10-18 19:02:00 -0400

    Persistence working.

--- app.py
+++ app.py
@@ -6,15 +6,49 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import DishwasherState
+from models import *
 
 @app.route('/')
-def hello():
-  return "Hello World!"
+def flask_dash():
+  #Get the latest DishwasherState by PK
+  latestState = DishwasherState.query.order_by(DishwasherState.id.desc()).first_or_404()
 
-@app.route('/<name>')
-def hello_name(name):
-  return "Hello {}!".format(name)
+  #If that state is clean
+  if latestState.is_clean:
+    return 'The dishes are clean'
+
+  #If that state is  dirty
+  else:
+    return 'The dishes are dirty'
+
+@app.route('/clean')
+def new_clean():
+  clean = DishwasherState(True)
+  db.session.add(clean)
+  db.session.commit()
+
+  return 'Dishes have been marked clean'
+
+@app.route('/dirty')
+def new_dirty():
+  dirty = DishwasherState(False)
+  db.session.add(dirty)
+  db.session.commit()
+
+  return 'Dishes have been marked dirty'
+
+@app.route('/toggle')
+def toggle_state():
+  #Get the latest DishwasherState by PK
+  latestState = DishwasherState.query.order_by(DishwasherState.id.desc()).first_or_404()
+
+  #Save the inverse of the current state
+  flipState = not latestState.is_clean
+  new = DishwasherState(flipState)
+  db.session.add(new)
+  db.session.commit()
+
+  return 'Dishwasher State has been toggled'
 
 if __name__ == '__main__':
   app.run()
